4b7a0e320a7e6df95e1202202301c124672f9429
Liebsch <Liebsch@R901AVKF-14-124.duerrdental.com>
2021-08-02 22:20:02 +0200

    - setLookAndFeel of crossplatform

--- Chat/src/milan/liebsch/chat/ChatServer.java
+++ Chat/src/milan/liebsch/chat/ChatServer.java
@@ -1,6 +1,9 @@
 package milan.liebsch.chat;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 import milan.liebsch.mycomponents.TitleLabel;
 
@@ -20,16 +23,26 @@ public class ChatServer extends JFrame {
 		setVisible(true);	
 	}
 	
-	
 
 	private void initGUI() {
+		// new instance of a titleLabel we want to use
 		TitleLabel titleLabel = new TitleLabel("Chat Server");
-		
+		// add our new titleLable to the GUI 
+		add(titleLabel, BorderLayout.BEFORE_FIRST_LINE);
 	}
 
 
 
 	public static void main(String[] args) {
+		try {
+			// find out the look and feel of the crossplatform
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			// set the look and feel of the crossplatform for the program gui
+			UIManager.setLookAndFeel(className);
+		}
+		catch(Exception e) {
+			// nothing todo until now
+		}
 		ChatServer test = new ChatServer();
 	}
 
